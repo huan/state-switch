3c5f70a08e3c9f37020530bdda9ffa8a6ab88f51
Huan (李卓桓) <zixia@zixia.net>
2021-10-23 14:52:04 +0800

    clean

---
+++ src/busy-indicator.ts
@@ -0,0 +1,42 @@
+import { StateSwitch }        from './state-switch.js'
+
+class BusyIndicator {
+
+  private state: StateSwitch
+
+  constructor (...args: ConstructorParameters<typeof StateSwitch>) {
+    this.state = new StateSwitch(...args)
+  }
+
+  /**
+   * Set busy state
+   * @param b busy or not
+   */
+  busy (b: boolean): void
+  /**
+   * Get busy state
+   */
+  busy (): boolean
+
+  busy (b?: boolean): void | boolean {
+    if (typeof b === 'undefined') {
+      return !!(this.state.on())
+    }
+
+    if (b) {
+      this.state.on(true)
+    } else {
+      this.state.off(true)
+    }
+  }
+
+  /**
+   * Return a Promise that resolves when the busy state is off
+   */
+  async available (): Promise<void> {
+    await this.state.ready('off')
+  }
+
+}
+
+export { BusyIndicator }
