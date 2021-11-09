3c2797c7529a4197117d58fc19deecf5a793750f
Huan (李卓桓) <zixia@zixia.net>
2021-11-10 00:07:26 +0800

    fix smoke testing to remove deprecated methods

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -6,10 +6,10 @@ import {
 const ss = new StateSwitch()
 console.info(`StateSwitch v${ss.version()}`)
 
-ss.on('pending')
-ss.on(true)
-ss.off('pending')
-ss.on(true)
+ss.active('pending')
+ss.active(true)
+ss.inactive('pending')
+ss.inactive(true)
 
 if (VERSION === '0.0.0') {
   throw new Error('version should be set before publishing')
